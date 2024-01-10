6b0c9f5d385d30db80eb64b2c79acb79c0aebc05
Pepkin <gospodinov.petko@icloud.com>
2024-01-10 16:28:24 +0200

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+          "primary": "#fbbf24",
+        
+          "secondary": "#ff5600",
+        
+          "accent": "#ff8400",
+        
+          "neutral": "#070a06",
+        
+          "base-100": "#f5f5f4",
+        
+          "info": "#00d3ff",
+        
+          "success": "#a3e635",
+        
+          "warning": "#dc2626",
+        
+          "error": "#ff0042",
+        },
+      },
+    ],
+  },
+}
+export default config
