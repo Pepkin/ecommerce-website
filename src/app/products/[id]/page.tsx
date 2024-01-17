ba2d1a6cb75dfd21e138e2e6b4eb2bdac2c7a966
Pepkin <gospodinov.petko@icloud.com>
2024-01-17 17:18:05 +0200

    Product page finished

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,56 @@
+import PriceTag from "@/components/PriceTag"
+import { prisma } from "@/lib/db/prisma"
+import { Metadata } from "next"
+import Image from "next/image"
+import { notFound } from "next/navigation"
+import { cache } from "react"
+
+interface ProdutPageProps {
+    params: {
+        id: string,
+    }
+}
+
+const getProdcut = cache(async (id: string) => {
+    const product = await prisma.product.findUnique({where: {id}})
+
+    if(!product){
+        notFound()
+    }
+
+    return product
+})
+
+export async function generateMetadata({params : {id}} : ProdutPageProps): Promise<Metadata> {
+    const product = await getProdcut(id)
+
+    return {
+        title: product.name + " - Pepozone",
+        description: product.description,
+        openGraph: {
+            images: [{url: product.imageUrl}]
+        }
+    }
+}
+
+export default async function ProductPage({params : {id}} : ProdutPageProps) {
+    
+    const product = await getProdcut(id)
+
+    return(
+        <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
+            <Image
+                src={product.imageUrl}
+                alt={product.name}
+                width={500}
+                height={500}
+                className="rounded-lg"
+            />
+            <div>
+                <h1 className="text-5xl font-bold">{product.name}</h1>
+                <PriceTag price={product.price} className="mt-4"/>
+                <p className="py-6">{product.description}</p>
+            </div>
+        </div>
+    )
+}
